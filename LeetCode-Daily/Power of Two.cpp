e61bdb9a1adf434617e1c757f8b0aaacf65bdfcc
Vanshika Pandey <56474909+vanshikapandey@users.noreply.github.com>
2021-12-21 03:51:21 -0500

    Leetcode Daily Solution

---
+++ LeetCode-Daily/Power of Two.cpp
@@ -0,0 +1,26 @@
+//Problem Link:https://leetcode.com/problems/power-of-two/
+
+
+//QUES
+
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ */
+
+/**/
+/***************CODE**************/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        if(n<=0){
+            return false;
+        }
+        if((n&(n-1))==0){
+            return true;
+        }
+        return false;
+    }
+};
